24f1cd02d250d86225cbdfd31fa3a67c93edb172
Andrew Rogers <andrewrogers@live.com>
2017-03-02 07:07:06 +0000

    support distinct, named eessions

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ var io = require('socket.io')(http);
 io.set('transports', [ 'websocket' ]);
 
 clients = [];
-clientCount = 0;
-
-/*
-// add a dummy client for testing purposes
-clients['test_id'] = {
-  'client_id' : 'test_id',
-  'name' : 'Testy McTestface',
-  'x' : 120,
-  'y' : 100
-};
-*/
 
 io.on('connection', registerClient);
 
@@ -23,33 +12,85 @@ function registerClient(socket) {
   socket.emit('hello', socket.id);
   console.log('hello ' + socket.id);
 
-  printClients(clients);
+  // register a handler for client join
+  socket.on("join", function(msg) {
+    console.log("join: " + msg);
 
-  // tell the new client about all existing clients
-  for (client in clients) {
-    var msg = JSON.stringify(clients[client]);
-    console.log('send to ' + socket.id + ': update ' + msg);
-    socket.emit('update', msg);
-  }
+    var join = JSON.parse(msg);
+
+    if (!join.name) {
+      var error = 'no name specified, join : ' + msg;
+      console.log(error);
+      socket.emit('error', error);
+      return;
+    }
+
+    if (!join.session) {
+      var error = 'no session specified, join : ' + msg;
+      console.log(error);
+      socket.emit("error", error);
+      return;
+    }
+
+    var player = {
+      name : join.name,
+      session : join.session,
+      client_id : socket.id,
+    };
+
+    clients[socket.id] = player;
+
+    // join the named ession
+    socket.join(join.session);
+
+    // tell the new player about all existing clients in the session
+    for (client_id in clients) {
+      var client = clients[client_id];
+      if (client.session === player.session) {
+        var msg = JSON.stringify(client);
+        console.log('send to ' + socket.id + ': update ' + msg);
+        socket.emit('update', msg);
+      }
+    }
+
+    printClients(clients);
+  });
 
   // register a handler for client updates
   socket.on('update', function(msg) {
     // notify all clients of the update
+    var client_id = socket.id;
+    var client = clients[client_id];
+    if (!client) {
+      var error = 'no client found, update: ' + msg;
+      console.log(error);
+      return;
+    }
+
     var update = JSON.parse(msg);
-    update.client_id = socket.id;
-    clients[socket.id] = update;
+    client.x = update.x;
+    client.y = update.y;
+    client.angle = update.angle;
+
+    update.name = client.name;
+    update.session = client.session;
+    update.client_id = client_id;
+
     var msg = JSON.stringify(update);
-    console.log('broadcast: update ' +  msg);
-    io.emit('update', msg);
-    //printClients(clients);
+    //console.log('update(' + client.session + '): ' +  msg);
+    io.sockets.in(client.session).emit('update', msg);
   });
 
   // register a handler for client disconnect
   socket.on('disconnect', function() {
     // notify all clients of the removal
-    console.log('broadcast: delete ' + socket.id);
-    io.emit('delete', socket.id);
-    delete clients[socket.id];
+    console.log('disconnect: ' + socket.id);
+    var client_id = socket.id;
+    var client = clients[client_id];
+    if (client) {
+      io.sockets.in(client.session).emit('delete', client_id);
+      delete clients[client_id];
+    }
     printClients(clients);
   });
 };
